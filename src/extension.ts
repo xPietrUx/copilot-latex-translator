82d63886acb68f9026579ece8afdc031597f7f9c
Piotr <piotrdusinski01@gmail.com>
2025-03-11 20:24:29 +0100

    Added import of setupWebviewCommunication

--- src/extension.ts
+++ src/extension.ts
@@ -1,4 +1,5 @@
 import * as vscode from 'vscode';
+import { setupWebviewCommunication } from './communication';
 
 export function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(
@@ -13,7 +14,11 @@ export function activate(context: vscode.ExtensionContext) {
         }
       );
 
+      // HTML content
       panel.webview.html = getWebViewContent();
+
+      // Communication between webview and extension
+      setupWebviewCommunication(panel, context);
     })
   );
 }
