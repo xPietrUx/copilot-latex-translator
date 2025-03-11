06ca4dab4e37deff69f156c9cb8b60202ea34061
Piotr <piotrdusinski01@gmail.com>
2025-03-11 20:24:06 +0100

    Implement webview communication setup in communication.ts

---
+++ src/communication.ts
@@ -0,0 +1,7 @@
+import * as vscode from 'vscode';
+
+// Communication between webview and extension
+export function setupWebviewCommunication(
+  panel: vscode.WebviewPanel,
+  context: vscode.ExtensionContext
+) {}
